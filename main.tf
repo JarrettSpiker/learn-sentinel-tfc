c0aeb19495f4c252bb54bd62c3b5e820f6d7433b
Jarrett Spiker <jarrettspiker@gmail.com>
2022-11-22 09:46:13 -0500

    add a bunch more instances

--- main.tf
+++ main.tf
@@ -33,3 +33,166 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+
+resource "aws_instance" "ubuntu1" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance1"
+  }
+}
+
+
+resource "aws_instance" "ubuntu2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance2"
+  }
+}
+
+
+resource "aws_instance" "ubuntu3" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance3"
+  }
+}
+
+
+resource "aws_instance" "ubuntu4" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance4"
+  }
+}
+
+
+resource "aws_instance" "ubuntu5" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance5"
+  }
+}
+
+
+resource "aws_instance" "ubuntu6" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance6"
+  }
+}
+
+
+resource "aws_instance" "ubuntu7" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance7"
+  }
+}
+
+
+resource "aws_instance" "ubuntu8" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance8"
+  }
+}
+
+
+
+resource "aws_instance" "ubuntu9" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance9"
+  }
+}
+
+
+resource "aws_instance" "ubuntu10" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance10"
+  }
+}
+
+
+resource "aws_instance" "ubuntu11" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance11"
+  }
+}
+
+
+resource "aws_instance" "ubuntu12" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance12"
+  }
+}
+
+
+resource "aws_instance" "ubuntu13" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance13"
+  }
+}
+
+
+resource "aws_instance" "ubuntu14" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance14"
+  }
+}
+
+
+resource "aws_instance" "ubuntu15" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance15"
+  }
+}
+
+
+resource "aws_instance" "ubuntu16" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "instance16"
+  }
+}
+
+
